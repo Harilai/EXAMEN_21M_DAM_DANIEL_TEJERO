ba6e95aa3585b4f69f978a11eec124b0f2776497
Usuario <Usuario@DESKTOP-TA74P1I>
2025-05-21 11:23:05 +0200

    Rama principal con lo basico

---
+++ src/EXAMEN_21M_PRIMO/Principal.java
@@ -0,0 +1,18 @@
+package EXAMEN_21M_PRIMO;
+
+import java.util.*;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Buenos dias ceinmark, soy Daniel Tejero :)");
+		System.out.println("Introduce un numero");
+		int numero = entrada.nextInt(); 	
+		
+		
+	}
+
+}
